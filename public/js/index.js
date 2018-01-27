8b7d657f45419978a591023cf90d76ecd9b7b555
icet00 <florent.enaux@gmail.com>
2018-01-27 16:04:29 +0100

    feat:added the compute works and fixed some errors

--- public/js/index.js
+++ public/js/index.js
@@ -5,17 +5,64 @@
   const render = (actors) => {
     const fragment = document.createDocumentFragment();
     const div = document.createElement('div');
-    const template = actors.map(actor => {
+    var table_head = [];
+    var temp =  `
+      <div class="col-md-12 col-sm-12">
+        <table class="table table-hover">
+          <thead>
+            <tr>`;
+    $.each(actors, function(index, value) {
+      temp += `<th scope="col">${value.who}</th>`;
+    }); 
+    temp += `
+            </tr>
+          </thead>
+          <tbody>
+            <tr>`;
+    $.each(actors, function(index, value) {
+      temp += `<td>${value.type}</td>`;
+    }); 
+    temp += `
+            </tr>
+            <tr>`;
+    $.each(actors, function(index, value) {
+      temp += `<td>${value.amount}</td>`;
+    }); 
+    temp += `
+            </tr>
+          </tbody>
+        </table>
+      </div>`;
+    /*const template = actors.map(actor => {
       return `
-        <div class="actor">
-          <span>${actor.who}</span>
-          <span>${actor.type}</span>
-          <span>${actor.amount}</span>
-        </div>
+      <div class="col-md-12 col-sm-12">
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>${actor.who}</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>${actor.type}</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>${actor.amount}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
       `;
-    }).join('');
+    }).join('');*/
 
-    div.innerHTML = template;
+    div.innerHTML = temp;
     fragment.appendChild(div);
     document.querySelector('#actors').innerHTML = '';
     document.querySelector('#actors').appendChild(fragment);
@@ -29,7 +76,6 @@
     const volume = document.querySelector('.volume').value;
     const option = document.querySelector('.option').checked;
     const actors = CONVARGO.payActors(trucker, distance, volume, option);
-
     render(actors);
 
     return;
